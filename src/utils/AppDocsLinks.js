6fadba9dd1ab49ffece57e910b666debbd203b9c
PayalWC <153177057+PayalWC@users.noreply.github.com>
2024-05-02 14:55:53 +0530

    Add App Document Links file

---
+++ src/utils/AppDocsLinks.js
@@ -0,0 +1,23 @@
+export const AppDocsLinks = {
+    BackInStockData: "https://wishlist.thimatic-apps.com/assets/images/BackInStockData.csv",
+    GetPostman: "https://documenter.getpostman.com/view/17366629/2s93eVVYTr",
+    WishListClubData: "https://wishlist.thimatic-apps.com/assets/images/WishListClubData.csv",
+    WriteReviewModal:"https://apps.shopify.com/wishlist-club#modal-show=WriteReviewModal",
+    facebook: "https://www.facebook.com/groups/238834717807393",
+    canny: "",
+    collection: {
+        "416": "https://webcontrive.helpscoutdocs.com/collection/416-wishlist-club",
+    },
+    article: {
+        "422": "https://webcontrive.helpscoutdocs.com/article/422-how-to-check-wishlist-product",
+        "424": "https://webcontrive.helpscoutdocs.com/article/424-how-to-customize-wishlist-button",
+        "423": "https://webcontrive.helpscoutdocs.com/article/423-wishlist-settings",
+        "425": "https://webcontrive.helpscoutdocs.com/article/425-wishlist-email-settings",
+        "426": "https://webcontrive.helpscoutdocs.com/article/426-wishlist-language-settings",
+        "515": "https://webcontrive.helpscoutdocs.com/article/515-how-to-check-email-sent-out-history",
+        "525": "https://webcontrive.helpscoutdocs.com/article/525-how-to-set-back-in-stock-button-and-email",
+    },
+    category: {
+        "430": "https://webcontrive.helpscoutdocs.com/category/430-general",
+    },
+}
